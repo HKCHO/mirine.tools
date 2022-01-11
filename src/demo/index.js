1ab4a1a7484a335e421aec955f2a116950dcb769
Cho HyunKwon <eddie88cho@clecue.com>
2022-01-11 13:24:36 +0900

    데모페이지 초기화

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,10 +1,1 @@
-import Mirine from "../lib";
-const mirineInstance = new Mirine();
-
-document.querySelector("body").innerHTML = `<h1>안녕, 세상!</h1>`;
-
-console.log("mirineInstance", mirineInstance);
-
-mirineInstance.testMethod();
-
-mirineInstance.saram.isRegistrationNum("23-094-23094-20394-20394");
+document.querySelector("body").innerHTML = `<h1>Mirine Javascript Tools</h1>`;
