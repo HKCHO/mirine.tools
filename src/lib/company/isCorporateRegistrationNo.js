01963d3ca26709511cd842ed7f838dda9ddedb9a
Cho HyunKwon <eddie88cho@clecue.com>
2022-01-12 16:51:41 +0900

    등록번호 유효성검사 법령 주석 추가

--- src/lib/company/isCorporateRegistrationNo.js
+++ src/lib/company/isCorporateRegistrationNo.js
@@ -3,8 +3,12 @@ import StringUtils from "../utils/string-utils";
 /**
  * 법인 등록번호 유효성 검사
  *
+ * 참고: [법인및재외국민의부동산등기용등록번호부여에관한규칙]{@link https://www.law.go.kr/LSW/lsInfoP.do?lsiSeq=57867#0000}
+ *
  * @param {string} registrationNo 법인등록번호
  * @returns {boolean} 법인등록번호 유효성
+ *
+ * @author hkcho
  */
 export function isCorporateRegistrationNo(registrationNo) {
     const rn = (registrationNo + "").trim().split("-").join(""); // 하이픈(-)이 있을경우 제거
