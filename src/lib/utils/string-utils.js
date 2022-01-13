ebdc171260f77668f5121ab431d677c7df2d5d69
hkcho <eddie88cho@gmail.com>
2022-01-13 17:01:56 +0900

    정규식 오류 수정 indentation 정리

--- src/lib/utils/string-utils.js
+++ src/lib/utils/string-utils.js
@@ -5,34 +5,36 @@
  * @author hkcho
  */
 class StringUtils {
-    constructor() {throw "인스턴스화 할 수 없습니다.";}
+  constructor() {
+    throw "인스턴스화 할 수 없습니다.";
+  }
 
-    /**
-     * 알파벳과 숫자로 이루어져 있는지 검사
-     *
-     * @param {string} str 검사할 문자열
-     *
-     * @author hkcho
-     */
-    static isAlphaNumeric(str) {
-        if (/\s/g.test(str) || /[^a-zA-Zs0-9]/g.test(str)) {
-            return false;
-        } else {
-            return /[\w\d]/g.test(str);
-        }
+  /**
+   * 알파벳과 숫자로 이루어져 있는지 검사
+   *
+   * @param {string} str 검사할 문자열
+   *
+   * @author hkcho
+   */
+  static isAlphaNumeric(str) {
+    if (/\s/g.test(str) || /[^a-zA-Z0-9]/g.test(str)) {
+      return false;
+    } else {
+      return /[\w\d]/g.test(str);
     }
+  }
 
-    /**
-     * 숫자 형식의 문자열인지 검사
-     *
-     * @param {string | number} str 검사할 문자열
-     *
-     * @author hkcho
-     */
-    static isNumeric(str) {
-        if (typeof str !== "string") return false;
-        return !isNaN(str) && !isNaN(parseFloat(str));
-    }
+  /**
+   * 숫자 형식의 문자열인지 검사
+   *
+   * @param {string | number} str 검사할 문자열
+   *
+   * @author hkcho
+   */
+  static isNumeric(str) {
+    if (typeof str !== "string") return false;
+    return !isNaN(str) && !isNaN(parseFloat(str));
+  }
 }
 
 export default StringUtils;
