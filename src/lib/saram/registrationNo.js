d0d287113ef0f3a0e9babe2412710ee3c8a344e1
hkcho <eddie88cho@gmail.com>
2022-01-17 17:52:10 +0900

    스타일가이드에 맞춤

--- src/lib/saram/registrationNo.js
+++ src/lib/saram/registrationNo.js
@@ -12,7 +12,7 @@ export { validateRegistrationNumber };
  * @returns {boolean} 주민/외국인 번호의 유효성
  */
 function validateRegistrationNumber(registrationNo, type) {
-  const rn = (`${registrationNo}`).trim().split('-').join(''); // 하이픈(-)이 있을경우 제거
+  const rn = `${registrationNo}`.trim().split('-').join(''); // 하이픈(-)이 있을경우 제거
 
   // 등록번호 형태의 문자열이 아님
   if (!StringUtils.isNumeric(rn) || rn.length !== 13) {
@@ -20,7 +20,8 @@ function validateRegistrationNumber(registrationNo, type) {
   }
 
   let checkSum = 0; // 검증값 합계
-  for (let i = 0; i < 12; i++) checkSum += (rn.substr(i, 1) >> 0) * ((i % 8) + 2);
+  for (let i = 0; i < 12; i++)
+    checkSum += (rn.substr(i, 1) >> 0) * ((i % 8) + 2);
 
   // 주민등록번호 매치
   const rrnMatch = `${(11 - (checkSum % 11)) % 10}` === rn.substr(12, 1);
