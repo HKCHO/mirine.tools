fb7303523c61125f666c850f1cf2b750cc91c984
hkcho <eddie88cho@gmail.com>
2022-01-18 22:10:07 +0900

    `airbnb`스타일가이드 적용

--- webpack.config.demo.js
+++ webpack.config.demo.js
@@ -1,33 +1,31 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    devtool: 'cheap-module-source-map',
-    entry: './src/demo/index.js',
-    output: {
-        filename: 'index.js'
-    },
-    optimization: {
-        minimize: false,
-    },
-    devServer: {
-        open: true,
-        hot: true,
-        host: "localhost",
-        port: 9000
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(m|j|t)s$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin(),
-    ]
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: './src/demo/index.js',
+  output: {
+    filename: 'index.js',
+  },
+  optimization: {
+    minimize: false,
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    host: 'localhost',
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(m|j|t)s$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
 };
